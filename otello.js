314b084616efb736d1c7a1a55ff0fc4956ae6497
saritazavala <42972998+saritazavala@users.noreply.github.com>
2020-02-03 20:24:44 -0600

    Matrix created yey xD

---
+++ otello.js
@@ -0,0 +1,18 @@
+/*
+Universidad del Valle de Guatemala
+Sara Nohemi Zavala Gutierrez
+18893 -- Laboratorio 2
+Febrero 05 de 2020
+* */
+
+const root = document.getElementById('root');
+const board__matrix =
+    [   [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, -1, 1, 0, 0, 0],
+        [0, 0, 0, 1, -1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0]
+    ];
